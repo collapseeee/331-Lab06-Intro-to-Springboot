968ea8184eaf0953fc5c35f73eee1f033af78812
COLLAPse <4174tt@gmail.com>
2026-08-31 21:17:13 +0700

    task 4.6: add response header

--- src/main/java/se331/lab/Application.java
+++ src/main/java/se331/lab/Application.java
@@ -13,10 +13,9 @@ public class Application {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/events")
-                        .allowedOrigins("http://localhost:5173");
-                registry.addMapping("/events/*")
-                        .allowedOrigins("http://localhost:5173");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .exposedHeaders("x-total-count");
             }
         };
     }
--- src/main/java/se331/lab/controller/EventController.java
+++ src/main/java/se331/lab/controller/EventController.java
@@ -1,6 +1,7 @@
 package se331.lab.controller;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -95,13 +96,15 @@ public class EventController {
         page = page == null ? 1 : page;
         Integer firstIndex = (page - 1) * perPage;
         List<Event> output = new ArrayList<>();
+        HttpHeaders responseHeader = new HttpHeaders();
+        responseHeader.set("x-total-count", String.valueOf(eventList.size()));
         try {
             for (int i = firstIndex; i < firstIndex + perPage; i++) {
                 output.add(eventList.get(i));
             }
-            return ResponseEntity.ok(output);
+            return ResponseEntity.ok().headers(responseHeader).body(output);
         } catch (IndexOutOfBoundsException e) {
-        return ResponseEntity.ok(output);
+            return ResponseEntity.ok().headers(responseHeader).body(output);
         }
     }
 
