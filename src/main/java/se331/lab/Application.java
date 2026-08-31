dc5f8268b0dbffa277ac801be9b5b81b28ffa91b
COLLAPse <4174tt@gmail.com>
2026-08-31 21:06:52 +0700

    task 4.3: add CORS policy for frontend

--- src/main/java/se331/lab/Application.java
+++ src/main/java/se331/lab/Application.java
@@ -2,12 +2,26 @@ package se331.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
+    @Bean
+    public WebMvcConfigurer corsConfiguerer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/events")
+                        .allowedOrigins("http://localhost:5173");
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
 }
+
